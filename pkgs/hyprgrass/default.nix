8353c39b3d6a02d2e276792c0d07d550191a9746
June Stepp <git@junestepp.me>
2026-07-28 10:42:09 -0500

    feat(hyprgrass): 0.55.4 -> 0.56.0

--- pkgs/hyprgrass/default.nix
+++ pkgs/hyprgrass/default.nix
@@ -2,7 +2,6 @@
   hyprgrass,
   wf-touch,
   fetchFromGitHub,
-  fetchpatch2,
 }:
 (hyprgrass.override {
   # See <https://github.com/NixOS/nixpkgs/pull/526498>.
@@ -15,21 +14,13 @@
 }).overrideAttrs
   (
     finalAttrs: prevAttrs: rec {
-      version = "0.55.4";
+      version = "0.56.0";
 
       src = fetchFromGitHub {
         owner = "horriblename";
         repo = "hyprgrass";
         tag = "hl-${version}";
-        hash = "sha256-tCt7FNc1RBHou/ym7B0XzoOqqNq8Df+dizEDkAgJ4U0=";
+        hash = "sha256-r0kKcEid5NcolUgfE7rI1TT3VAMxrnjqzGLIVs/lbI8=";
       };
-
-      patches = prevAttrs.patches or [ ] ++ [
-        # See <https://github.com/horriblename/hyprgrass/issues/391>.
-        (fetchpatch2 {
-          url = "https://github.com/horriblename/hyprgrass/commit/230495900cef1a5681bf8f8abf797939d1d64c1b.diff?full_index=1";
-          hash = "sha256-/YStL0sGnffO1whDAfMmtH0qO3gp2j/WPNLhrLLQpeQ=";
-        })
-      ];
     }
   )
